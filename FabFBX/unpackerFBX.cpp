f5dd77983ccad894c1931670edf97b35bc1fd1e5
Fabrizio Corda <cordafab@gmail.com>
2020-12-28 16:48:05 +0100

    Remove spaces from skeleton bones names

--- FabFBX/unpackerFBX.cpp
+++ FabFBX/unpackerFBX.cpp
@@ -481,6 +481,11 @@ void UnpackerFBX::saveSkeleton (const std::string                 & filename,
 
    for( unsigned long i = 0; i < names.size(); ++i )
    {
+
+      //Remove spaces from joint names
+      std::string name = names[i];
+      std::replace(name.begin(), name.end(), ' ', '_');
+
       fp << "j "
          << i << " "
          << names[i] << " "
